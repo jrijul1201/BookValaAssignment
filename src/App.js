e99897d39e61c7f7d0dd10a8f71f4ceb5cfbdb59
jrijul1201 <jrijul1201@gmail.com>
2022-11-25 20:34:42 +0530

    Assignment for BookVala

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import AsyncSelect from 'react-select/async';
+import { colourOptions } from './Options';
+import { colourStyles } from './Styles';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const filterColors = (inputValue) => {
+  return colourOptions.filter((i) =>
+    i.label.toLowerCase().includes(inputValue.toLowerCase())
   );
-}
+};
 
-export default App;
+const promiseOptions = (inputValue) =>
+  new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(filterColors(inputValue));
+    }, 1000);
+  });
+
+export default () => (
+  <AsyncSelect
+    isMulti
+    cacheOptions
+    defaultOptions
+    loadOptions={promiseOptions}
+    styles={colourStyles}
+  />
+);
